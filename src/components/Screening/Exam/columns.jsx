55ce40ad254a0148660eb9903c453bd971f67037
Marcelo Arocha <marceloarocha@gmail.com>
2020-06-11 15:02:23 -0300

    screening: basic exams history chart

--- src/components/Screening/Exam/columns.jsx
+++ src/components/Screening/Exam/columns.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 import { toDataSource } from '@utils';
 import Table, { NestedTableContainer } from '@components/Table';
 import Empty from '@components/Empty';
+import Chart from 'react-google-charts';
+import LoadBox from '@components/LoadBox';
 
 const columns = [
   {
@@ -65,20 +68,52 @@ export const expandedExamRowRender = record => {
   const history = record.history.map((item, index) => ({ ...item, key: index }));
 
   const dsHistory = toDataSource(history, 'key');
+  const graphDataArray = history.map(item => {
+    return [item.date, parseFloat(item.value.match(/[\d|,|.|e|E|\+]+/g)[0], 10)]; // eslint-disable-line
+  });
+  const dsGraph = [['data', 'valor']].concat(graphDataArray.reverse());
 
   return (
     <NestedTableContainer>
-      <Table
-        columns={expandedColumns}
-        pagination={false}
-        locale={{
-          emptyText: (
-            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Nenhum exame encontrado." />
-          )
-        }}
-        dataSource={dsHistory}
-        rowClassName={examRowClassName}
-      />
+      <Row gutter={8} justify="center">
+        <Col md={12}>
+          <Table
+            columns={expandedColumns}
+            pagination={false}
+            locale={{
+              emptyText: (
+                <Empty
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                  description="Nenhum exame encontrado."
+                />
+              )
+            }}
+            dataSource={dsHistory}
+            rowClassName={examRowClassName}
+          />
+        </Col>
+        <Col md={12}>
+          {dsGraph.length > 2 && (
+            <Chart
+              width={'100%'}
+              height={'400px'}
+              chartType="LineChart"
+              loader={<LoadBox />}
+              data={dsGraph}
+              options={{
+                hAxis: {
+                  title: 'Data'
+                },
+                vAxis: {
+                  title: 'Valor'
+                },
+                backgroundColor: 'transparent'
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />
+          )}
+        </Col>
+      </Row>
     </NestedTableContainer>
   );
 };
