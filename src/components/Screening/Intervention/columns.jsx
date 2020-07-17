c0aed21d1cb4b0752eba9f561ee716e0044c65b2
Henrique Dias <heukirne@gmail.com>
2020-07-17 12:51:35 -0300

    init future button

--- src/components/Screening/Intervention/columns.jsx
+++ src/components/Screening/Intervention/columns.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 import { format } from 'date-fns';
 
 import Icon from '@components/Icon';
-import Button from '@components/Button';
+import Button, { Link } from '@components/Button';
 import Descriptions from '@components/Descriptions';
 import Tag from '@components/Tag';
 import Menu from '@components/Menu';
@@ -66,6 +66,18 @@ export const InterventionView = ({ intervention, showReasons, showDate, status }
     {status}
     <Descriptions.Item label="Prescrição:" span={3}>
       <PrescriptionInline {...intervention} />
+    </Descriptions.Item>
+    <Descriptions.Item label="Prescrição Porterior:" span={3}>
+      {isEmpty(intervention.future) && (
+        <Link
+          onClick={() => intervention.fetchPeriod(intervention.idPrescriptionDrug)}
+          loading={intervention.isFetching}
+          type="nda gtm-bt-iterv-future"
+        >
+          Visualizar prescrição posterior
+        </Link>
+      )}
+      {!isEmpty(intervention.future) && intervention.future}
     </Descriptions.Item>
     {showDate && (
       <Descriptions.Item label="Data" span={3}>
