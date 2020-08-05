fcb7679a067b7c8e8555621a5addcd5bc349bc04
Marcelo Arocha <marceloarocha@gmail.com>
2020-08-04 21:56:25 -0300

    basic renders without crashing test

--- src/components/Inputs/DatePicker.jsx
+++ src/components/Inputs/DatePicker.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'antd/lib/date-picker/style/index.css';
 import AntDatePicker from 'antd/lib/date-picker';
-import locale from 'antd/es/date-picker/locale/pt_BR';
+import locale from 'antd/lib/date-picker/locale/pt_BR';
 
 export const DatePicker = props => <AntDatePicker locale={locale} {...props} />;
--- src/components/Login/index.test.jsx
+++ src/components/Login/index.test.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './';
 
-it('renders with Result', () => {
-  expect(1).toBe(1); // dummy test
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Login isLogging={false} doLogin={() => false} match={{ params: {} }} />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
