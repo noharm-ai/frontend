38617101c1306d213f0dc880a72878f80e2a7bd8
Henrique Dias <heukirne@gmail.com>
2020-10-09 12:06:30 -0300

    change to sec version

--- src/utils/appInfo.js
+++ src/utils/appInfo.js
@@ -1,5 +1,5 @@
 const appName = 'NoHarm.ai';
-const version = 'v1.28-beta';
+const version = 'v1.27.sec-beta';
 const currentYear = new Date().getFullYear();
 const copyright = `${appName} ${currentYear} ${version}`;
 
