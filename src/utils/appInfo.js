01761287bf22d6ef8a1edee0250afaecbec372ee
Henrique Dias <heukirne@gmail.com>
2020-11-28 13:43:28 -0300

    add tag colors

--- src/utils/appInfo.js
+++ src/utils/appInfo.js
@@ -1,5 +1,5 @@
 const appName = 'NoHarm.ai';
-const version = 'v1.33-beta';
+const version = 'v1.34-beta';
 const currentYear = new Date().getFullYear();
 const copyright = `${appName} ${currentYear} ${version}`;
 
