07ed02b5f109b1d06656b1380a61e39dc78e0fbd
Marcelo Arocha <marceloarocha@gmail.com>
2021-01-23 19:12:08 -0300

    clinical notes: message when empty

--- src/components/Screening/ClinicalNotes/index.jsx
+++ src/components/Screening/ClinicalNotes/index.jsx
@@ -5,6 +5,7 @@ import { format, parseISO } from 'date-fns';
 import { Row, Col } from '@components/Grid';
 import Tag from '@components/Tag';
 import LoadBox from '@components/LoadBox';
+import Empty from '@components/Empty';
 
 import { Container, Paper, List, PaperHeader } from './index.style';
 
@@ -39,78 +40,40 @@ export default function ClinicalNotes({ isFetching, list, selected, select }) {
         </Col>
         <Col md={10} xl={8} className="list-panel">
           <List>
-            <>
-              {Object.keys(list).map(g => (
-                <React.Fragment key={g}>
-                  <h2>{format(parseISO(g), 'dd/MM/yyyy')}</h2>
-                  <div className="line-group">
-                    {list[g].map((c, i) => (
-                      <div
-                        className={`line ${selected && c.id === selected.id ? 'active' : ''}`}
-                        key={i}
-                        onClick={() => select(c)}
-                      >
-                        <div className="time">{c.date.substr(11, 5)}</div>
-                        <div className="name">
-                          {c.prescriber}
-                          <span>{c.position}</span>
+            {Object.keys(list).length === 0 && (
+              <Empty
+                image={Empty.PRESENTED_IMAGE_SIMPLE}
+                description="Nenhuma evolução encontrada"
+              />
+            )}
+            {Object.keys(list).length > 0 && (
+              <>
+                {Object.keys(list).map(g => (
+                  <React.Fragment key={g}>
+                    <h2>{format(parseISO(g), 'dd/MM/yyyy')}</h2>
+                    <div className="line-group">
+                      {list[g].map((c, i) => (
+                        <div
+                          className={`line ${selected && c.id === selected.id ? 'active' : ''}`}
+                          key={i}
+                          onClick={() => select(c)}
+                        >
+                          <div className="time">{c.date.substr(11, 5)}</div>
+                          <div className="name">
+                            {c.prescriber}
+                            <span>{c.position}</span>
+                          </div>
+                          <div className="indicators">
+                            <Tag color="orange">5</Tag>
+                            <Tag color="red">2</Tag>
+                          </div>
                         </div>
-                        <div className="indicators">
-                          <Tag color="orange">5</Tag>
-                          <Tag color="red">2</Tag>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </React.Fragment>
-              ))}
-              {/* <h2>12/01/2021</h2>
-
-            <div className="line-group">
-              <div className="line active">
-                <div className="time">18:20</div>
-                <div className="name">
-                  Asdrubal Nelson
-                  <span>Médico | CRM 123132</span>
-                </div>
-                <div className="indicators">
-                  <Tag color="orange">5</Tag>
-                  <Tag color="red">2</Tag>
-                </div>
-              </div>
-
-              <div className="line">
-                <div className="time">16:20</div>
-                <div className="name">
-                  Asdrubal Nelson
-                  <span>Médico | CRM 123132</span>
-                </div>
-                <div className="indicators">
-                  <Tag color="orange">5</Tag>
-                  <Tag color="red">2</Tag>
-                </div>
-              </div>
-
-              <div className="line">
-                <div className="time">13:05</div>
-                <div className="name">
-                  Asdrubal Nelson
-                  <span>Médico | CRM 123132</span>
-                </div>
-                <div className="indicators">
-                  <Tag color="orange">5</Tag>
-                </div>
-              </div>
-              <div className="line">
-                <div className="time">09:10</div>
-                <div className="name">
-                  Asdrubal Nelson
-                  <span>Médico | CRM 123132</span>
-                </div>
-                <div className="indicators" />
-              </div>
-            </div> */}
-            </>
+                      ))}
+                    </div>
+                  </React.Fragment>
+                ))}
+              </>
+            )}
           </List>
         </Col>
       </Row>
