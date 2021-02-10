513f24dd82b2ee06da28c29f3012c49e1d668206
Marcelo Arocha <marceloarocha@gmail.com>
2021-02-10 20:27:06 -0300

    annotation: move annotation to another component

--- src/components/Screening/ClinicalNotes/index.jsx
+++ src/components/Screening/ClinicalNotes/index.jsx
@@ -1,139 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import isEmpty from 'lodash.isempty';
+import React from 'react';
 import { format, parseISO } from 'date-fns';
 
 import { Row, Col } from '@components/Grid';
 import LoadBox from '@components/LoadBox';
 import Empty from '@components/Empty';
-import { useOutsideAlerter } from '@lib/hooks';
 
-import ClinicalNotesIndicator from './ClinicalNotesIndicator';
-import { Container, Paper, List, PaperHeader, PaperContainer, MenuPopup } from './index.style';
+import View from './View';
+import { Container, List } from './index.style';
 
 export default function ClinicalNotes({ isFetching, list, selected, select, update }) {
-  const paperContainerRef = useRef(null);
-  const menuRef = useRef(null);
-  const [isMenuVisible, setMenuVisibility] = useState(false);
-  const [menuPosition, setMenuPosition] = useState({});
-  const [selectionRange, setSelectionRange] = useState(null);
-
-  useOutsideAlerter(menuRef, () => {
-    setMenuVisibility(false);
-  });
-
-  useEffect(() => {
-    if (!isMenuVisible && selectionRange) {
-      const textNode = document.createTextNode(selectionRange.toString());
-      selectionRange.deleteContents();
-      selectionRange.insertNode(textNode);
-    }
-  }, [isMenuVisible]); //eslint-disable-line
-
-  const annotate = option => {
-    const elm = document.createElement('span');
-    const close = document.createElement('a');
-    const content = document.createTextNode(selectionRange.toString());
-
-    close.setAttribute('class', 'close-btn');
-    close.appendChild(document.createTextNode('X'));
-
-    elm.setAttribute('class', `annotation annotation-${option.key}`);
-    elm.appendChild(content);
-    elm.appendChild(close);
-
-    selectionRange.deleteContents();
-    selectionRange.insertNode(elm);
-
-    setSelectionRange(null);
-    setMenuVisibility(false);
-    update({ id: selected.id, text: paperContainerRef.current.firstChild.innerHTML });
-  };
-
-  const removeAnnotation = e => {
-    const el = e.target.closest('SPAN');
-    if (el && e.target.className === 'close-btn') {
-      el.removeChild(el.getElementsByClassName('close-btn')[0]);
-      el.replaceWith(document.createTextNode(el.innerText));
-      update({ id: selected.id, text: paperContainerRef.current.firstChild.innerHTML });
-    }
-  };
-
-  const isValidSelection = () => {
-    const selection = window.getSelection();
-
-    if (selection.toString() === '') return false;
-
-    if (selection.focusNode.nodeName !== '#text') return false;
-
-    const range = selection.getRangeAt(0);
-
-    if (range.cloneContents().querySelectorAll('span').length) {
-      return false;
-    }
-
-    if (range.commonAncestorContainer.offsetParent !== undefined) {
-      const { className } = range.commonAncestorContainer.offsetParent;
-      if (!className.includes('Paper-') && !className.includes('PaperContainer')) {
-        console.log(
-          'invalid selection',
-          range.commonAncestorContainer.offsetParent.className,
-          range
-        );
-        return false;
-      }
-    }
-
-    return true;
-  };
-
-  const selectionChange = () => {
-    if (isValidSelection()) {
-      const selection = window.getSelection();
-      const range = selection.getRangeAt(0);
-
-      const elm = document.createElement('span');
-      const content = document.createTextNode(window.getSelection().toString());
-
-      elm.appendChild(content);
-
-      range.deleteContents();
-      range.insertNode(elm);
-
-      const containerPosition = paperContainerRef.current.getBoundingClientRect();
-      const menuContainerPosition = menuRef.current.getBoundingClientRect();
-      const elmPosition = elm.getBoundingClientRect();
-
-      const top = elmPosition.top - containerPosition.top - menuContainerPosition.height;
-
-      setSelectionRange(range);
-      setMenuVisibility(true);
-      setMenuPosition({
-        left: elmPosition.left - containerPosition.left + elmPosition.width,
-        top: top < 0 ? -20 : top
-      });
-    }
-  };
-
-  const menu = (
-    <div
-      style={{
-        position: 'absolute',
-        top: menuPosition.top,
-        left: menuPosition.left,
-        visibility: isMenuVisible ? 'visible' : 'hidden'
-      }}
-      ref={menuRef}
-    >
-      <MenuPopup theme="dark" onClick={k => annotate(k)} selectable={false}>
-        {ClinicalNotesIndicator.list().map(i => (
-          <MenuPopup.Item key={i.value}>
-            <div className="avatar" style={{ backgroundColor: i.color }} /> {i.label}
-          </MenuPopup.Item>
-        ))}
-      </MenuPopup>
-    </div>
-  );
-
   if (isFetching) {
     return (
       <Container>
@@ -146,31 +21,7 @@ export default function ClinicalNotes({ isFetching, list, selected, select, upda
     <Container>
       <Row type="flex" gutter={0}>
         <Col md={14} xl={16} className="paper-panel">
-          {!isEmpty(selected) && (
-            <>
-              <PaperHeader>
-                <div className="line">
-                  <div className="info">
-                    {format(parseISO(selected.date), 'dd/MM/yyyy HH:mm')} -{' '}
-                    <span className="name">{selected.prescriber}</span>
-                  </div>
-                </div>
-              </PaperHeader>
-              <PaperContainer ref={paperContainerRef}>
-                <>
-                  <Paper
-                    dangerouslySetInnerHTML={{
-                      __html: selected.text.trim().replaceAll('  ', '<br/>')
-                    }}
-                    onMouseUp={e => selectionChange(e)}
-                    onClick={e => removeAnnotation(e)}
-                    className={isMenuVisible ? 'disabled' : ''}
-                  />
-                  {menu}
-                </>
-              </PaperContainer>
-            </>
-          )}
+          <View selected={selected} update={update} />
         </Col>
         <Col md={10} xl={8} className="list-panel">
           <List>
@@ -191,6 +42,7 @@ export default function ClinicalNotes({ isFetching, list, selected, select, upda
                           className={`line ${selected && c.id === selected.id ? 'active' : ''}`}
                           key={i}
                           onClick={() => select(c)}
+                          aria-hidden="true"
                         >
                           <div className="time">{c.date.substr(11, 5)}</div>
                           <div className="name">
