b4940a7122028c9b7382bc26620dc37815c45559
Marcelo Arocha <marceloarocha@gmail.com>
2021-07-08 20:39:38 -0300

    improve expand row performance

---
+++ src/components/Screening/PrescriptionDrug/components/Table.jsx
@@ -0,0 +1,50 @@
+import React, { useState, memo } from 'react';
+
+import { ExpandableTable } from '@components/Table';
+import Empty from '@components/Empty';
+
+import columnsTable, { expandedRowRender, solutionColumns } from '../../columns';
+import { rowClassName } from '../PrescriptionDrugList';
+
+function Table({ hasFilter, filter, bag, isFetching, emptyMessage, ds, listType }) {
+  const [expandedRows, setExpandedRows] = useState([]);
+
+  const updateExpandedRows = (list, key) => {
+    if (list.includes(key)) {
+      return list.filter(i => i !== key);
+    }
+
+    return [...list, key];
+  };
+
+  const handleRowExpand = record => {
+    setExpandedRows(updateExpandedRows(expandedRows, record.key));
+  };
+
+  const extraBag = {
+    ...bag,
+    handleRowExpand
+  };
+
+  return (
+    <ExpandableTable
+      columns={
+        listType === 'solution'
+          ? solutionColumns(extraBag)
+          : columnsTable(hasFilter ? filter : { status: null }, extraBag)
+      }
+      pagination={false}
+      loading={isFetching}
+      locale={{
+        emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={emptyMessage} />
+      }}
+      dataSource={!isFetching ? ds.value : []}
+      expandedRowRender={expandedRowRender(extraBag)}
+      rowClassName={rowClassName}
+      expandedRowKeys={expandedRows}
+      onExpand={(expanded, record) => handleRowExpand(record)}
+    />
+  );
+}
+
+export default Table;
