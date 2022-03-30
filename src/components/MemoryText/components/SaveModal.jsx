5ad4e4c4145553217d776b5a9a575628be1de7f9
Marcelo <marceloarocha@gmail.com>
2022-03-29 22:55:03 -0300

    gtm: clinical notes - user templates

---
+++ src/components/MemoryText/components/SaveModal.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import Heading from '@components/Heading';
+import Modal from '@components/Modal';
+import { Input } from '@components/Inputs';
+
+export default function SaveModal({ save, open, setOpen }) {
+  const [name, setName] = useState('');
+
+  const saveAction = () => {
+    save(name);
+    setOpen(false);
+    setName('');
+  };
+
+  return (
+    <Modal
+      visible={open}
+      onCancel={() => setOpen(false)}
+      onOk={() => saveAction()}
+      okButtonProps={{
+        disabled: name === ''
+      }}
+      okText="Salvar"
+      okType="primary gtm-bt-memorytext-savemodal"
+      cancelText="Cancelar"
+    >
+      <Heading as="label" size="14px" className="fixed" style={{ marginTop: '12px' }}>
+        Nome do texto padrão:
+      </Heading>
+      <Input onChange={({ target }) => setName(target.value)} value={name} maxLength={50} />
+    </Modal>
+  );
+}
