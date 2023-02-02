3f0bc45e3b7fcba99d52a434985e94579507af66
Marcelo <marceloarocha@gmail.com>
2023-02-02 17:25:47 -0300

    v2.02-beta

--- src/utils/appInfo.js
+++ src/utils/appInfo.js
@@ -1,5 +1,5 @@
 const appName = "NoHarm.ai";
-const version = "v2.01-beta";
+const version = "v2.02-beta";
 const currentYear = new Date().getFullYear();
 const copyright = `${appName} ${currentYear} ${version}`;
 
