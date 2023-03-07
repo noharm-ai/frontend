c97d54a79bbbe3c096b244b714b5499f2a6e15cf
Marcelo <marceloarocha@gmail.com>
2023-03-07 20:10:03 -0300

    exams: help tooltip

---
+++ src/components/Help.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { QuestionOutlined } from "@ant-design/icons";
+
+import Tooltip from "./Tooltip";
+
+export default function ({ text }) {
+  return (
+    <Container>
+      <Tooltip title={text}>
+        <QuestionOutlined />
+      </Tooltip>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  border: 1px solid #2e3c5a;
+  border-radius: 50%;
+  display: inline-block;
+  padding: 0px 5px;
+  font-size: 14px;
+  margin-left: 12px;
+`;
