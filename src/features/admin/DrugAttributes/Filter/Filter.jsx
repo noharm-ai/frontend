147cb379dabde5fdf696e0ac38c38b7348729f88
Marcelo Arocha <marceloarocha@gmail.com>
2023-10-17 21:00:30 -0300

    admin drugs: reorder filters

--- src/features/admin/DrugAttributes/Filter/Filter.jsx
+++ src/features/admin/DrugAttributes/Filter/Filter.jsx
@@ -72,6 +72,7 @@ export default function Filter({ limit }) {
         "hasPriceConversion",
         "hasSubstance",
         "hasDefaultUnit",
+        "hasPriceUnit",
         "idSegmentList",
       ]}
     />
