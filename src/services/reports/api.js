e7e4d92b6a0d18797dc8f321441ddd9821ce323b
Marcelo Arocha <marceloarocha@gmail.com>
2024-03-18 22:13:50 -0300

    reports: dynamic date limit

--- src/services/reports/api.js
+++ src/services/reports/api.js
@@ -1,9 +1,5 @@
 import { instance, setHeaders } from "services/api";
 
-const endpoints = {
-  general: "/reports/general",
-};
-
 const getConfig = (params = {}) =>
   instance.get(`/reports/config`, {
     params,
@@ -16,39 +12,11 @@ const getReport = (report, params = {}) =>
     ...setHeaders(),
   });
 
-const getPatientDay = (params = {}) =>
-  instance.get(`${endpoints.general}/patient-day`, {
-    params,
-    ...setHeaders(),
-  });
-
-const getPrescription = (params = {}) =>
-  instance.get(`${endpoints.general}/prescription`, {
-    params,
-    ...setHeaders(),
-  });
-
-const getPrescriptionAudit = (params = {}) =>
-  instance.get(`${endpoints.general}/audit`, {
-    params,
-    ...setHeaders(),
-  });
-
-const getIntervention = (params = {}) =>
-  instance.get(`${endpoints.general}/intervention`, {
-    params,
-    ...setHeaders(),
-  });
-
 const getCache = (url) => {
   return instance.get(url, { responseType: "arraybuffer", decompress: true });
 };
 
 const api = {
-  getPatientDay,
-  getPrescription,
-  getPrescriptionAudit,
-  getIntervention,
   getReport,
   getCache,
   getConfig,
