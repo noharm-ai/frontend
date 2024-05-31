41a2f7ab6564f4b69ee510a486717a9b1d59db39
Marcelo Arocha <marceloarocha@gmail.com>
2024-05-31 22:26:29 +0200

    reports: history adjusts

--- src/features/reports/PrescriptionReport/Filter/Filter.jsx
+++ src/features/reports/PrescriptionReport/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import dayjs from "dayjs";
-import { FloatButton, Spin, Alert, Button } from "antd";
+import { FloatButton, Spin } from "antd";
 import {
   MenuOutlined,
   PrinterOutlined,
