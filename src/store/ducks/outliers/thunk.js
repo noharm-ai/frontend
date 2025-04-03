496acf1bf4babb3c0c5c654e92484cc1a85d9a6d
Marcelo Arocha <marceloarocha@gmail.com>
2025-04-03 16:42:44 -0300

    fix null param error

--- src/store/ducks/outliers/thunk.js
+++ src/store/ducks/outliers/thunk.js
@@ -150,6 +150,11 @@ export const fetchReferencesListThunk =
       params.f = frequency;
     }
 
+    if (!params.idDrug || !params.idSegment) {
+      dispatch(outliersFetchListError("Medicamento inválido"));
+      return;
+    }
+
     const {
       data: { data },
       error,
