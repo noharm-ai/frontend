bccbd456062c3508a8004ad93a93317f4778e88e
Marcelo Arocha <marceloarocha@gmail.com>
2026-02-25 16:16:44 -0300

    remove deprecated dependency

--- src/styles/utils.js
+++ src/styles/utils.js
@@ -1,10 +1,11 @@
-import { get as getter } from "lodash";
 import { rem as polishedRem, stripUnit } from "polished";
 
 import { defaultFontSize } from "styles/sizes";
 
-export const get = (val, defaultValue) => (p) =>
-  getter(p, `theme.${val}`, defaultValue);
+export const get = (val, defaultValue) => (p) => {
+  const result = val.split(".").reduce((acc, key) => acc?.[key], p?.theme);
+  return result !== undefined ? result : defaultValue;
+};
 
 export const rem = (size) =>
   polishedRem(stripUnit(size), stripUnit(defaultFontSize));
